938dd4301f0d41597a2f3bda08ec730f0dd24f28
MikeZarazua <medinazarazuamike@gmail.com>
2019-06-02 16:01:22 -0500

    Initial Commit

---
+++ MVPArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVPArchitecture
+//
+//  Created by Miguel Zarazua on 6/2/19.
+//  Copyright © 2019 Miguel Zarazua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
